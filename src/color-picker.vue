a7863bdf6cde138dc091ed30288399df814c841d
Nino Vrijman <ninovrijman@gmail.com>
2018-10-30 10:15:51 +0100

    Debug off by default

--- src/color-picker.vue
+++ src/color-picker.vue
@@ -73,7 +73,7 @@ export default {
     },
     data() {
         return {
-            debug: true,
+            debug: false,
             dragging: false,
             circleDrag: false,
             value: '',
