cb43f2ca5c54cdad552a89be4f02682bb09aa6fd
Nino Vrijman <ninovrijman@gmail.com>
2018-11-16 10:13:23 +0100

    Added postcss with autoprefixer to rollup config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 import { uglify } from "rollup-plugin-uglify";
+import Autoprefixer from 'autoprefixer';
 
 export default {
     input: 'src/wrapper.js', // Path relative to package.json
@@ -12,9 +13,12 @@ export default {
         vue({
             css: true, // Dynamically inject css as a <style> tag
             compileTemplate: true, // Explicitly convert template to render function
+            style: {
+                postcssPlugins: [Autoprefixer()]
+            }
             // Use style: 'scss' ? https://rollup-plugin-vue.vuejs.org/options.html#defaultlang
         }),
         buble(), // Transpile to ES5
-        process.env.SB_MINIFY ? uglify() : undefined
+        process.env.SB_MINIFY ? uglify() : undefined,
     ],
 };
